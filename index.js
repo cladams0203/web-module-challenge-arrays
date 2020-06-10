c9fbd8c32c10f736636fd9a9a855085f179d9555
cladams0203 <cladams0203@gmail.com>
2020-06-10 12:13:49 -0400

    finished task 6

--- index.js
+++ index.js
@@ -117,10 +117,11 @@ Your function should accept:
 2 arguments 1 for your new array and one for your original array
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
-
-function copy(/*code here*/) {
-  /*code here*/
+function copy(arry, copyArray) {
+  return (copyArray = [...arry]);
 }
+const newArry = [];
+console.log(copy(originalFlavors, newArry));
 
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
